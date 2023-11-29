4816965411ec97680b4e69d40e61cfe8187494a2
flaneur3li <fmy21373061@buaa.edu.cn>
2023-11-29 20:45:31 +0800

    commit

--- backend/app/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from app.models import *
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
--- backend/app/views/view_set.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from app.models import *
-from app.serializer import *
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
